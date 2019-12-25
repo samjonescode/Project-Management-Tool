166b0b98a236e1a8e111a8af683edf9958f383ed
Sam <samuelriley1393@gmail.com>
2019-12-25 13:27:49 -0500

    implemented userValidator

--- Personal-Project-Management-Tool/src/main/java/com/ppmtool/controllers/UserController.java
+++ Personal-Project-Management-Tool/src/main/java/com/ppmtool/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.ppmtool.controllers;
 import com.ppmtool.domain.User;
 import com.ppmtool.services.MapValidationErrorService;
 import com.ppmtool.services.UserService;
+import com.ppmtool.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,15 +25,19 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserValidator userValidator;
+
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result){
         //Validate passwords match
-
+        userValidator.validate(user,result);
         ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
         if(errorMap.getStatusCode()== HttpStatus.BAD_REQUEST) return errorMap;
 
-        User newUser = userService.saveUser(user);
 
+        User newUser = userService.saveUser(user);
+        newUser.setConfirmPassword(""); // hide the confirm pw in response
         return new ResponseEntity<User>(newUser, HttpStatus.CREATED);
 
     }
