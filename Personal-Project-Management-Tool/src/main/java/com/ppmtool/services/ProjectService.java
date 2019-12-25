9b28d2e32c7071bc5be2ffe814d219456f693c12
Sam <samuelriley1393@gmail.com>
2019-12-25 15:54:48 -0500

    jwt implemented

--- Personal-Project-Management-Tool/src/main/java/com/ppmtool/services/ProjectService.java
+++ Personal-Project-Management-Tool/src/main/java/com/ppmtool/services/ProjectService.java
@@ -4,6 +4,7 @@ import com.ppmtool.domain.Backlog;
 import com.ppmtool.domain.Project;
 import com.ppmtool.domain.User;
 import com.ppmtool.exceptions.ProjectIdException;
+import com.ppmtool.exceptions.ProjectNotFoundException;
 import com.ppmtool.repositories.BacklogRepository;
 import com.ppmtool.repositories.ProjectRepository;
 import com.ppmtool.repositories.UserRepository;
@@ -47,25 +48,30 @@ public class ProjectService {
 		}
 	}
 
-	public Project findProjectByIdentifier(String projectId){
-		Project project = projectRepository.findByProjectIdentifier(projectId);
+	public Project findProjectByIdentifier(String projectId, String username){
+		Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
 		if (project == null) {
 			throw new ProjectIdException(("Project does '" + projectId +"'not exist."));
+		}
+
+		if (!project.getProjectLeader().equals(username)){ //if person fetching isnot creator
+			throw new ProjectNotFoundException("Project not in your account");
 		}
 		return projectRepository.findByProjectIdentifier(projectId.toUpperCase());
 	}
 
-	public Iterable<Project> findAllProjects(){
-		return projectRepository.findAll();
+	public Iterable<Project> findAllProjects(String username){
+		return projectRepository.findAllByProjectLeader(username);
 	}
 
-	public void deleteProjectByIdentifier(String projectId){
-		Project project = projectRepository.findByProjectIdentifier(projectId);
+	public void deleteProjectByIdentifier(String projectId, String username){
+//		Project project = projectRepository.findByProjectIdentifier(projectId);
+//
+//		if(project == null){
+//			throw new ProjectIdException(("Cannot delete project with ID'"+projectId +"'. This project does not exist."));
+//		}
+		System.out.println("Inside delete method of proj service");
 
-		if(project == null){
-			throw new ProjectIdException(("Cannot delete project with ID'"+projectId +"'. This project does not exist."));
-		}
-
-		projectRepository.delete(project);
+		projectRepository.delete(findProjectByIdentifier(projectId,username));
 	}
 }
