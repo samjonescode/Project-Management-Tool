ce8af24aa7c80802a9ed09cec0d419adc9a3334a
Sam <samuelriley1393@gmail.com>
2019-12-25 14:00:02 -0500

    jwt implemented

--- Personal-Project-Management-Tool/src/main/java/com/ppmtool/controllers/UserController.java
+++ Personal-Project-Management-Tool/src/main/java/com/ppmtool/controllers/UserController.java
@@ -1,12 +1,19 @@
 package com.ppmtool.controllers;
 
+import com.ppmtool.controllers.payload.JWTLoginSuccessResponse;
+import com.ppmtool.controllers.payload.LoginRequest;
 import com.ppmtool.domain.User;
+import com.ppmtool.security.JwtTokenProvider;
 import com.ppmtool.services.MapValidationErrorService;
 import com.ppmtool.services.UserService;
 import com.ppmtool.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +21,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+
+import static com.ppmtool.security.SecurityConstants.TOKEN_PREFIX;
 
 @RestController
 @RequestMapping("/api/users")
@@ -27,6 +36,34 @@ public class UserController {
 
     @Autowired
     private UserValidator userValidator;
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest,
+                                              BindingResult result){
+        ResponseEntity<?> errorMap = MapValidationErrorService.MapValidationService(result);
+        if(errorMap.getStatusCode().equals(HttpStatus.BAD_REQUEST)){
+            return errorMap;
+        }
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = TOKEN_PREFIX + tokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JWTLoginSuccessResponse(true, jwt));
+
+    }
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result){
--- Personal-Project-Management-Tool/src/main/java/com/ppmtool/security/SecurityConfig.java
+++ Personal-Project-Management-Tool/src/main/java/com/ppmtool/security/SecurityConfig.java
@@ -1,12 +1,21 @@
 package com.ppmtool.security;
 
+import com.ppmtool.services.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static com.ppmtool.security.SecurityConstants.H2_URL;
+import static com.ppmtool.security.SecurityConstants.SIGN_UP_URLS;
 
 @Configuration
 @EnableWebSecurity
@@ -17,11 +26,29 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 )
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+
     @Autowired
     private JWTAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Autowired
+    CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Override
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        super.configure(http);
         http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
                 .sessionManagement()
@@ -40,7 +67,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/**/*.css",
                         "/**/*.js"
                 ).permitAll()
-                .antMatchers("/api/users/**").permitAll()
+                .antMatchers(SIGN_UP_URLS).permitAll() // permit access to user urls without auth/sign in
+                .antMatchers(H2_URL).permitAll() //permit access to h2 without sign in
                 .anyRequest().authenticated(); //other than these routes everything needs authentication
 
 
